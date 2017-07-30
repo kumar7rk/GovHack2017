197cb47fff360a62d940665d9cc0ba0183cb6696
Rohit Kumar <rohitkumarrk1992@gmail.com>
2017-07-30 15:33:27 +0930

    Maps Activity- information added
    Screen showing information on the map itself using marker

--- app/src/main/java/com/geeky7/rohit/govhack2017/MapsActivity.java
+++ app/src/main/java/com/geeky7/rohit/govhack2017/MapsActivity.java
@@ -2,6 +2,7 @@ package com.geeky7.rohit.govhack2017;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,9 +28,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34.921064,138.594303);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng loc = new LatLng(-26.2060033,134.4391496);
+        LatLng WA = new LatLng(-27.638,124.362);
+        LatLng NT = new LatLng(-20.420,135.041);
+        LatLng QLD = new LatLng(-23.550,143.245);
+        LatLng NSW = new LatLng(-32.615,146.994);
+        LatLng VIC = new LatLng(-38.611,143.391);
+        LatLng SA = new LatLng(-29.510,135.964);
+        LatLng TAS = new LatLng(-41.936,145.620);
+
+        mMap.addMarker(new MarkerOptions().position(WA).title("TOTAL REFUGEES 13,346 (9.7%)"));
+        mMap.addMarker(new MarkerOptions().position(NT).title("Total Refugees 1,618 (1.2%)"));
+        mMap.addMarker(new MarkerOptions().position(QLD).title("Total Refugees 17,441 (12.7%)"));
+        mMap.addMarker(new MarkerOptions().position(NSW).title("Total Refugees 49,585 (36%)"));
+        mMap.addMarker(new MarkerOptions().position(VIC).title("Total Refugees 42,079 (30.6%)"));
+        mMap.addMarker(new MarkerOptions().position(SA).title("Total Refugees 7,500(5.5%)"));
+        mMap.addMarker(new MarkerOptions().position(TAS).title("Total Refugees 1,468 (1.3%)"));
+
+        mMap.setContentDescription("Total Refugees");
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+        mMap.getCameraPosition();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 3.5f));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(3.5f), 2000, null);
+        Toast.makeText(getApplicationContext(),"SomeData "+mMap.getMinZoomLevel(),Toast.LENGTH_SHORT);
     }
 }
